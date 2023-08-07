21b2b5718927a0016c5a0d102a0f705d7ac50e27
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-07 17:16:49 -0300

    Java Packages

--- trilha_basico/java-anatomia-classes/src/edu/barata/anatomiadasclasses/BoletimEstudantil.java
+++ trilha_basico/java-anatomia-classes/src/edu/barata/anatomiadasclasses/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.barata.anatomiadasclasses;
+
 public class BoletimEstudantil {
   public static void main(String[] args) {
     int mediaFinal = 5;
--- trilha_basico/java-anatomia-classes/src/edu/barata/anatomiadasclasses/MinhaClasse.java
+++ trilha_basico/java-anatomia-classes/src/edu/barata/anatomiadasclasses/MinhaClasse.java
@@ -1,3 +1,5 @@
+package edu.barata.anatomiadasclasses;
+
 public class MinhaClasse {
   public static void main(String[] args) {
 
