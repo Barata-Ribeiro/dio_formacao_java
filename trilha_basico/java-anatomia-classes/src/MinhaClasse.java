b92b0e78dc545678d809cac69503b3ee8c6ac5a9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-07 08:19:43 -0300

    Convenção e nomenclaturas de variaveis

--- trilha_basico/java-anatomia-classes/src/MinhaClasse.java
+++ trilha_basico/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,15 @@
 public class MinhaClasse {
   public static void main(String[] args) {
-    System.out.print("Hello, world! I'm eager to launch my first Java software!");
+
+    // int ano = 2021;
+    // ano = 2023;
+    // int ESTADOS_BRASILEIRO = 27; // Convenção, imutável
+    // final String BR = "Brasil"; // Convenção, imutável
+
+    // NOMENCLATURAS VARIAVEIS
+    // int numero$um = 1;
+    // String nomePessoa = "João";
+    // String _sobrenome = "Bourne";
+    // int $idade = 27;
   }
 }
