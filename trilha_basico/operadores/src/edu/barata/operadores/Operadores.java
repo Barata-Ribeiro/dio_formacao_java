e197e989b519bf139532d205f46aae7d2196609e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-09 10:14:46 -0300

    Operadores d atribuição e aritméticos

---
+++ trilha_basico/operadores/src/edu/barata/operadores/Operadores.java
@@ -0,0 +1,44 @@
+package edu.barata.operadores;
+
+public class Operadores {
+  public static void main(String[] args) {
+
+    // Atribuição
+    String nome = "Barata";
+    int idade = 27;
+    double peso = 89.7;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    // Date dataNascimento = new Date();
+
+    // Aritiméticos
+    double soma = 10.5 + 15.7;
+    int subtração = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20 / 4);
+
+    // classe Operadores.java
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+    System.out.println(nomeCompleto);
+
+    // qual o resultado das expressoes abaixo?
+    String concatenacao = "?";
+
+    concatenacao = 1 + 1 + 1 + "1"; // 31
+    System.out.println(concatenacao);
+
+    concatenacao = 1 + "1" + 1 + 1; // 1111
+    System.out.println(concatenacao);
+
+    concatenacao = 1 + "1" + 1 + "1"; // 1111
+    System.out.println(concatenacao);
+
+    concatenacao = "1" + 1 + 1 + 1; // 1111
+    System.out.println(concatenacao);
+
+    concatenacao = "1" + (1 + 1 + 1); // 13
+    System.out.println(concatenacao);
+  }
+}
