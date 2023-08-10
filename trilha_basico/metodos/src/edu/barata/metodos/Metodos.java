2e59a0acd71f2ad1936d4ae46ebd44f143c22cfc
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-10 16:24:18 -0300

    Conceitos Metodos

---
+++ trilha_basico/metodos/src/edu/barata/metodos/Metodos.java
@@ -0,0 +1,17 @@
+package edu.barata.metodos;
+
+public class Metodos {
+  // Forma de chamar um método
+  // somar(int n1, int n2){}
+
+  // abrirConexao(){}
+
+  // concluirProcessamento() {}
+
+  // findById(int id){} // não se assuste, você verá muito método em inglês em sua
+  // jornada
+
+  // calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma
+  // única finalidade
+
+}
