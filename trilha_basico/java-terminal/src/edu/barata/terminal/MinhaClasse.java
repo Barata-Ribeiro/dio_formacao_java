dacb05d6c782ee9e66af218080bfa14d94f0dde2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-11 16:42:40 -0300

    Usando Terminal

---
+++ trilha_basico/java-terminal/src/edu/barata/terminal/MinhaClasse.java
@@ -0,0 +1,9 @@
+package edu.barata.terminal;
+
+public class MinhaClasse {
+  public static void main(String[] args) {
+    System.out.println("Estou sendo executado pelo terminal!");
+
+    System.out.println("Outro códígo sendo executado pelo terminal!");
+  }
+}
