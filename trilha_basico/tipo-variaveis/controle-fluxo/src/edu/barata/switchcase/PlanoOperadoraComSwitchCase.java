ce10d726b3da7cdad4ad39b6bb12ee74e52a64be
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-15 07:47:43 -0300

    Switch Case

---
+++ trilha_basico/tipo-variaveis/controle-fluxo/src/edu/barata/switchcase/PlanoOperadoraComSwitchCase.java
@@ -0,0 +1,19 @@
+package edu.barata.switchcase;
+
+public class PlanoOperadoraComSwitchCase {
+  public static void main(String[] args) {
+    String plano = "T"; // M / T
+
+    switch (plano) {
+      case "T": {
+        System.out.println("5Gb Youtube");
+      }
+      case "M": {
+        System.out.println("WhatsApp e Instagram grátis");
+      }
+      case "B": {
+        System.out.println("100 minutos de ligação");
+      }
+    }
+  }
+}
