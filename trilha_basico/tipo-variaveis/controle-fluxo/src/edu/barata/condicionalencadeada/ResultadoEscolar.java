90897e5f52efd75f0498b82d95cd293ac40115fa
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-15 07:32:09 -0300

    Condicionais Encateadas

---
+++ trilha_basico/tipo-variaveis/controle-fluxo/src/edu/barata/condicionalencadeada/ResultadoEscolar.java
@@ -0,0 +1,15 @@
+package edu.barata.condicionalencadeada;
+
+// ResultadoEscolar.java
+public class ResultadoEscolar {
+  public static void main(String[] args) {
+    int nota = 6;
+
+    if (nota >= 7) // Retorna true ou false
+      System.out.println("Aprovado");
+    else if (nota >= 5 && nota < 7) // Retorna true ou false
+      System.out.println("Recuperação");
+    else // Se for false, então executa o código abaixo
+      System.out.println("Reprovado");
+  }
+}
