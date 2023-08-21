a41cafc098d540ec8f17b8dc2ab14f8b2238d067
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-21 07:27:59 -0300

    Estrutura Pacotes service

---
+++ trilha_basico/orientacao-a-objetos/src/edu/barata/pacotes/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,8 @@
+package edu.barata.pacotes.com.mastersoft.clinical.service;
+
+import edu.barata.pacotes.com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+  void salvarExame(Exame exame) {
+  }
+}
