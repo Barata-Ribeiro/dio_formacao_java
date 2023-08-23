8182c728171a0e301a600a6a0bb9c7dca14a86e1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-23 07:26:09 -0300

    Utilizando Getters & Setters

--- trilha_basico/orientacao-a-objetos/src/edu/barata/gettersesetters/Escola.java
+++ trilha_basico/orientacao-a-objetos/src/edu/barata/gettersesetters/Escola.java
@@ -1,5 +1,20 @@
 package edu.barata.gettersesetters;
 
 public class Escola {
-  
+  public static void main(String[] args) {
+    Aluno felipe = new Aluno();
+    felipe.setNome("Felipe");
+    felipe.setIdade(8);
+    felipe.setSexo("Masculino");
+
+    System.out.println(
+        "O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos e seu sexo é " + felipe.getSexo() + ".");
+
+    Aluno Alice = new Aluno();
+    Alice.setNome("Alice");
+    Alice.setIdade(10);
+    Alice.setSexo("Feminino");
+    System.out.println(
+        "A aluna " + Alice.getNome() + " tem " + Alice.getIdade() + " anos e seu sexo é " + Alice.getSexo() + ".");
+  }
 }
