20456011da1938f875e85c61ae341afa3024bf4c
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-24 07:08:18 -0300

    Iniciando em POO

---
+++ trilha_basico/orientacao-a-objetos/src/edu/dio/pilarespoo/Autodromo.java
@@ -0,0 +1,7 @@
+package edu.dio.pilarespoo;
+
+public class Autodromo {
+  public static void main(String[] args) {
+    Carro formulaOne = new Carro();
+  }
+}
---
+++ trilha_basico/orientacao-a-objetos/src/edu/dio/pilarespoo/Carro.java
@@ -0,0 +1,5 @@
+package edu.dio.pilarespoo;
+
+public class Carro {
+  
+}
