5832e9c599419d6caa06b3f9591f7d142ed3f5be
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-24 07:33:10 -0300

    Pilar Encapsulamento

---
+++ trilha_basico/orientacao-a-objetos/src/edu/dio/praticapilares/Computador.java
@@ -0,0 +1,12 @@
+package edu.dio.praticapilares;
+
+public class Computador {
+  public static void main(String[] args) {
+    // abrindo MSN Messenger
+    MSNMessenger msn = new MSNMessenger();
+
+    msn.enviarMensagem();
+
+    msn.receberMensagem();
+  }
+}
