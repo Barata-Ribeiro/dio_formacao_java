afd9f2c08f37d7739808fabb3b13bfeed2ceedf1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-26 10:07:29 -0300

    Movido pasta do desafio 'iPhone2007'

--- project_challenges/iPhone2007_desafio/src/com/apple/utilities/PhotoUtils.java
+++ project_challenges/iPhone2007_desafio/src/com/apple/utilities/PhotoUtils.java
@@ -1,13 +1,13 @@
 package com.apple.utilities;
 
 public class PhotoUtils {
-  private String[] photoDatabase = new String[100]; // Simulando um banco de dados para até 100 fotos
-  private int photoCount = 0; // Contador de fotos
+  private String[] photoDatabase = new String[100]; // Database simulation with a max of 100 photos
+  private int photoCount = 0; // Photo Counter
 
   public void takePhoto(String photoName) {
     if (photoCount < photoDatabase.length) {
       photoDatabase[photoCount] = photoName;
-      photoCount++;
+      photoCount += 1;
       System.out.println("Photo taken: " + photoName);
     } else {
       System.out.println("Cannot take more photos. Photo database is full.");
@@ -15,10 +15,10 @@ public class PhotoUtils {
   }
 
   public void deletePhoto(String photoName) {
-    for (int i = 0; i < photoCount; i++) {
+    for (int i = 0; i < photoCount; i += 1) {
       if (photoDatabase[i].equals(photoName)) {
         // Shift remaining elements to fill the gap
-        for (int j = i; j < photoCount - 1; j++) {
+        for (int j = i; j < photoCount - 1; j += 1) {
           photoDatabase[j] = photoDatabase[j + 1];
         }
         photoCount--;
@@ -30,7 +30,7 @@ public class PhotoUtils {
   }
 
   public void selectPhoto(String photoName) {
-    for (int i = 0; i < photoCount; i++) {
+    for (int i = 0; i < photoCount; i += 1) {
       if (photoDatabase[i].equals(photoName)) {
         System.out.println("Selected photo: " + photoName);
         return;
@@ -41,7 +41,7 @@ public class PhotoUtils {
 
   public void listPhotos() {
     System.out.println("List of photos:");
-    for (int i = 0; i < photoCount; i++) {
+    for (int i = 0; i < photoCount; i += 1) {
       System.out.println(photoDatabase[i]);
     }
   }
