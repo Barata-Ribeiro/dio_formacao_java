d770f114a120e5b570eb3e78a5c1eacdfdfdd221
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-29 08:03:08 -0300

    Conceito e Encadeamento de Nó

---
+++ trilha_estrutura-dados/projetos/src/main/java/com/projeto/projetono/No.java
@@ -0,0 +1,38 @@
+package com.projeto.projetono;
+
+public class No {
+
+  private String conteudo;
+  private No proximoNo;
+
+  public No(String conteudo) {
+    this.proximoNo = null;
+    this.conteudo = conteudo;
+  }
+
+  // GETTERS AND SETTERS
+  public String getConteudo() {
+    return conteudo;
+  }
+
+  public void setConteudo(String conteudo) {
+    this.conteudo = conteudo;
+  }
+
+  public No getProximoNo() {
+    return proximoNo;
+  }
+
+  public void setProximoNo(No proximoNo) {
+    this.proximoNo = proximoNo;
+  }
+
+  // TO STRING
+  @Override
+  public String toString() {
+    return "No{" +
+        "conteudo='" + conteudo + '\'' +
+        '}';
+  }
+
+}
