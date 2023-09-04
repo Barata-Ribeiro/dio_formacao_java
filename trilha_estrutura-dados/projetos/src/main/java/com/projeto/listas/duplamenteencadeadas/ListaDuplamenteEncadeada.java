c7f69a6063f681d346f1b703e51107251e531ffb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-04 06:56:55 -0300

    Implementação da classe ListaDuplamenteEncadeada (add, add(index))

--- trilha_estrutura-dados/projetos/src/main/java/com/projeto/listas/duplamenteencadeadas/ListaDuplamenteEncadeada.java
+++ trilha_estrutura-dados/projetos/src/main/java/com/projeto/listas/duplamenteencadeadas/ListaDuplamenteEncadeada.java
@@ -17,6 +17,42 @@ public class ListaDuplamenteEncadeada<T> {
     return this.getNo(index).getConteudo();
   }
 
+  public void add(T elemento) {
+    NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+    novoNo.setNoProximo(null);
+    novoNo.setNoAnterior(this.ultimoNo);
+
+    if (this.primeiroNo == null)
+      this.primeiroNo = novoNo;
+    if (this.ultimoNo != null)
+      this.ultimoNo.setNoProximo(novoNo);
+
+    this.ultimoNo = novoNo;
+    this.tamanhoLista += 1;
+  }
+
+  public void add(int index, T elemento) {
+    NoDuplo<T> noAuxiliar = getNo(index);
+    NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+
+    novoNo.setNoProximo(noAuxiliar);
+
+    if (novoNo.getNoProximo() != null) {
+      novoNo.setNoAnterior(noAuxiliar.getNoAnterior());
+      novoNo.getNoProximo().setNoAnterior(novoNo);
+    } else {
+      novoNo.setNoAnterior(this.ultimoNo);
+      this.ultimoNo = novoNo;
+    }
+
+    if (index == 0)
+      this.primeiroNo = novoNo;
+    else
+      novoNo.getNoAnterior().setNoProximo(novoNo);
+
+    tamanhoLista += 1;
+  }
+
   private NoDuplo<T> getNo(int index) {
     NoDuplo<T> noAuxiliar = this.primeiroNo;
 
