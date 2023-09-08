f4e2bc07f9d95c05b9a59241075e7726f9af06bb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-08 07:42:25 -0300

    Implementação da classe NoArvore - Intro

---
+++ trilha_estrutura-dados/projetos/src/main/java/com/projeto/arvorebinaria/BinNo.java
@@ -0,0 +1,14 @@
+package com.projeto.arvorebinaria;
+
+public class BinNo<T extends Comparable<T>> {
+
+  private T conteudo;
+  private BinNo<T> noEsq;
+  private BinNo<T> noDir;
+
+  public BinNo(T conteudo) {
+    this.conteudo = conteudo;
+    this.noEsq = this.noDir = null;
+  }
+
+}
