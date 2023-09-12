f6ad9b12048f2f1c60e21fcd5da8caeb03f6ba67
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-12 06:41:23 -0300

    Trabalhando com Collections Java - List

---
+++ trilha_collections/estudos-colecoes/src/main/java/com/trilhacollections/list/ExemploList.java
@@ -0,0 +1,11 @@
+package com.trilhacollections.list;
+
+public class ExemploList {
+
+  public static void main(String[] args) {
+
+    System.out.println("Crie uma lsita e adicione as sete notas: ");
+
+  }
+
+}
