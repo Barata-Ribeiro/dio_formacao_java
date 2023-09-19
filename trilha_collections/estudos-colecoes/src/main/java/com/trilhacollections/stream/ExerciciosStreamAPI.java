a216bddceb281a245be52d48ad11402bd425b18d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-19 07:23:10 -0300

    Principais operações Stream API, parte 1

---
+++ trilha_collections/estudos-colecoes/src/main/java/com/trilhacollections/stream/ExerciciosStreamAPI.java
@@ -0,0 +1,55 @@
+package com.trilhacollections.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ExerciciosStreamAPI {
+
+  public static void main(String[] args) {
+    List<String> numerosAleatorios = Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
+
+    System.out.println("Imprima todos os elementos dessa lista de String: ");
+    numerosAleatorios.forEach(System.out::println);
+
+    System.out.println();
+
+    System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set:");
+    numerosAleatorios.stream()
+        .limit(5)
+        .collect(Collectors.toSet())
+        .forEach(System.out::println);
+
+    System.out.println();
+
+    System.out.println("Transforme esta lista de String em uma lista de números inteiros.");
+    List<Integer> numerosAleatorios2 = numerosAleatorios.stream()
+        .map(Integer::parseInt)
+        .collect(Collectors.toList());
+    numerosAleatorios2.forEach(System.out::println);
+
+    System.out.println();
+
+    System.out.println("Pegue os números pares e maiores que 2 e coloque em uma lista:");
+
+    System.out.println("Mostre a média dos números: ");
+
+    System.out.println("Remova os valores ímpares: ");
+
+    // Para você
+    System.out.println("Ignore os 3 primeiros elementos da lista e imprima o restante:");
+
+    System.out.println("Retirando os números repetidos da lista, quantos números ficam? ");
+
+    System.out.print("Mostre o menor valor da lista: ");
+
+    System.out.print("Mostre o maior valor da lista: ");
+
+    System.out.println("Pegue apenas os números pares e some: ");
+
+    System.out.println("Mostre a lista na ordem númerica: ");
+
+    System.out.println("Agrupe os valores ímpares múltiplos de 3 ou de 5:");
+  }
+
+}
