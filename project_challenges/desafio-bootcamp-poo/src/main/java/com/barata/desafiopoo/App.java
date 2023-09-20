8a51eec001b7002295e5858d07a70b02772719c1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-20 06:45:11 -0300

    Abstração e Encapsulamento

--- project_challenges/desafio-bootcamp-poo/src/main/java/com/barata/desafiopoo/App.java
+++
@@ -1,8 +0,0 @@
-package com.barata.desafiopoo;
-
-public class App {
-    
-    public static void main(String[] args) {
-    }
-    
-}
---
+++ project_challenges/desafio-bootcamp-poo/src/main/java/com/barata/desafiopoo/Main.java
@@ -0,0 +1,34 @@
+package com.barata.desafiopoo;
+
+import java.time.LocalDate;
+
+import com.barata.desafiopoo.dominio.Curso;
+import com.barata.desafiopoo.dominio.Mentoria;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Curso cursoUm = new Curso();
+    cursoUm.setTitulo("Curso Java");
+    cursoUm.setDescricao("Curso de formação Java.");
+    cursoUm.setCargaHoraria(120);
+
+    Curso cursoDois = new Curso();
+    cursoDois.setTitulo("Curso TypeScript");
+    cursoDois.setDescricao("Curso de formação em TypeScript.");
+    cursoDois.setCargaHoraria(80);
+
+    System.out.println(cursoUm);
+    System.out.println(cursoDois);
+
+    Mentoria mentoriaJava = new Mentoria();
+    mentoriaJava.setTitulo("Mentoria de Java");
+    mentoriaJava.setDescricao("Mentoria para a formação de devs em Java.");
+    mentoriaJava.setData(LocalDate.now());
+
+    System.out.println(mentoriaJava);
+
+  }
+
+}
