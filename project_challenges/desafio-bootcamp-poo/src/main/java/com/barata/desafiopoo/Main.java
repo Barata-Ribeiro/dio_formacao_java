648ae9ef62eff6d60236d994d3adaa6af65f554b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-20 07:02:21 -0300

    Herança e Polimorfismo, parte 1

--- project_challenges/desafio-bootcamp-poo/src/main/java/com/barata/desafiopoo/Main.java
+++ project_challenges/desafio-bootcamp-poo/src/main/java/com/barata/desafiopoo/Main.java
@@ -11,12 +11,12 @@ public class Main {
 
     Curso cursoUm = new Curso();
     cursoUm.setTitulo("Curso Java");
-    cursoUm.setDescricao("Curso de formação Java.");
+    cursoUm.setDescricao("Curso de formação Java");
     cursoUm.setCargaHoraria(120);
 
     Curso cursoDois = new Curso();
     cursoDois.setTitulo("Curso TypeScript");
-    cursoDois.setDescricao("Curso de formação em TypeScript.");
+    cursoDois.setDescricao("Curso de formação em TypeScript");
     cursoDois.setCargaHoraria(80);
 
     System.out.println(cursoUm);
@@ -24,7 +24,7 @@ public class Main {
 
     Mentoria mentoriaJava = new Mentoria();
     mentoriaJava.setTitulo("Mentoria de Java");
-    mentoriaJava.setDescricao("Mentoria para a formação de devs em Java.");
+    mentoriaJava.setDescricao("Mentoria para a formação de devs em Java");
     mentoriaJava.setData(LocalDate.now());
 
     System.out.println(mentoriaJava);
