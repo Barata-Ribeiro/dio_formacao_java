4ca47298c0698c0a25a8b78aea52f5ebc21c8680
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-26 06:20:57 -0300

    Criando um projeto via linha de comando

---
+++ trilha_dependencias-e-builds/Maven/quick-start-maven/src/main/java/com/barataribeiro/App.java
@@ -0,0 +1,15 @@
+package com.barataribeiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+// Created using mvn archetype:generate -B "-DarchetypeArtifactId=maven-archetype-quickstart" "-DarchetypeGroupId=org.apache.maven.archetypes" "-DgroupId=com.barataribeiro" "-DartifactId=quick-start-maven" "-DinteractiveMode=false"
