69c999c405910b94f1e3a2e0293e647282aa7afc
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-10 06:51:27 -0300

    Unchecked exception

--- trilha_qualidade_boas-praticas/exceptions-java/src/br/com/dio/exceptions/UncheckedException.java
+++ trilha_qualidade_boas-praticas/exceptions-java/src/br/com/dio/exceptions/UncheckedException.java
@@ -5,11 +5,34 @@ import javax.swing.*;
 //Fazer a divisão de 2 valores inteiros
 public class UncheckedException {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
+        boolean continuePrograma = true;
 
-        int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-        System.out.println("Resultado: " + resultado);
+        do {
+
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+
+                JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+
+                System.out.println("Resultado: " + resultado);
+
+                continuePrograma = false;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Entrada inválida. Digite apenas números inteiros! \n\n" + "Exception: " + e.getMessage());
+                // e.printStackTrace();
+            } catch (ArithmeticException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Entrada inválida. Não é possível dividir um número por zero (0)!");
+            } finally {
+                System.out.println("Chegou no 'finally'...");
+            }
+        } while (continuePrograma);
+
+        System.out.println("Código continua...");
     }
 
     public static int dividir(int a, int b) {
