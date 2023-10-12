3aefa5a3184ed76bb5f7944815c0cedd7c3d5dbb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-12 07:45:38 -0300

    Checked exception, parte 2

--- trilha_qualidade_boas-praticas/exceptions-java/src/br/com/dio/exceptions/CheckedException.java
+++ trilha_qualidade_boas-praticas/exceptions-java/src/br/com/dio/exceptions/CheckedException.java
@@ -5,14 +5,38 @@ import java.io.*;
 
 //Imprimir um arquivo no console.
 public class CheckedException {
-    public static void main(String[] args) throws IOException {
-        String nomeDoArquivo = "romance-blake-crouch.txt";
-        imprimirArquivoNoConsole(nomeDoArquivo);
+    public static void main(String[] args) {
+        String nomeDoArquivo = "romances-blake-crouch.txt";
+
+        try {
+            imprimirArquivoNoConsole(nomeDoArquivo);
+        } catch (NullPointerException e) {
+            if (nomeDoArquivo.equals("") || nomeDoArquivo.equals(null)) {
+                JOptionPane.showMessageDialog(null,
+                        "Caminho do arquivo não fornecido. \n\n" + e.getMessage(), "Erro!",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Revise o nome do arquivo que você desejar imprimir. \n\n" + e.getMessage(), "Erro!",
+                    JOptionPane.ERROR_MESSAGE);
+            // e.printStackTrace();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Não foi possível abrir o arquivo. \n\n" + e.getMessage() + e.getCause(), "Erro!",
+                    JOptionPane.ERROR_MESSAGE);
+            // e.printStackTrace();
+        } finally {
+            System.out.println("Chegou ao fim do programa.");
+        }
 
         System.out.println("Apesar da exception ou não, o programa continua...");
     }
 
     public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
+        if (nomeDoArquivo == null || nomeDoArquivo.isEmpty())
+            throw new NullPointerException("Nome do arquivo é nulo ou vazio");
+
         File file = new File(nomeDoArquivo);
 
         BufferedReader br = new BufferedReader(new FileReader(file.getName()));
