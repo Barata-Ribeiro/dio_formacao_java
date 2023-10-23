1cca9bf7a3b16160d40f14a8222f6cc24d30a7af
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-23 06:57:05 -0300

    Na prática: I/O Caracteres - Exercício 1

--- trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOCharacter/Exercicio1IOCaracter.java
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOCharacter/Exercicio1IOCaracter.java
@@ -1,12 +1,43 @@
 package br.com.dio.java.io.IOCharacter;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 //Abra o teclado para escrever 3 filmes que você recomendaria e exiba esses filmes no console.
 public class Exercicio1IOCaracter {
 
-    public static void main(String[] args) throws IOException {
+    public static void receberTecladoImprimirConsole() throws IOException {
 
+        // InputStream is = System.in;
+        // InputStreamReader isr = new InputStreamReader(is);
+        // BufferedReader br = new BufferedReader(isr);
+
+        System.out.println("Recomende 3 filmes: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String readLine = br.readLine();
+
+        // OutputStream os = System.out;
+        // OutputStreamWriter osw = new OutputStreamWriter(os);
+        // BufferedWriter bw = new BufferedWriter(osw);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do {
+            bw.write(readLine);
+            bw.newLine();
+            readLine = br.readLine();
+        } while (!readLine.isEmpty());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        receberTecladoImprimirConsole();
     }
 
 }
