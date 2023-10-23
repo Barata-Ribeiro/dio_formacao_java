2da09814fa24b209e3e79a6c53b390048b072f8d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-23 06:57:39 -0300

    Na prática: I/O Caracteres - Exercício 2 Inicio

---
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOCharacter/Exercicio2IOCaracter.java
@@ -0,0 +1,11 @@
+package br.com.dio.java.io.IOCharacter;
+
+import java.io.IOException;
+
+//Abra o teclado para escrever 3 filmes que você recomendaria e armazene em “recomendacoes.txt”.
+public class Exercicio2IOCaracter {
+
+    public static void main(String[] args) throws IOException {
+    }
+
+}
