37c0247b146ea19fc10e15f10b6e7363225b8fd2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-25 06:20:23 -0300

    Na prática: I/O Bytes - Inicio

---
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/ExemploIOBytes.java
@@ -0,0 +1,13 @@
+package br.com.dio.java.io.IOBytes;
+
+import java.io.*;
+
+//Faça uma cópia do arquivo “recomendacoes-copy.txt”.
+public class ExemploIOBytes {
+    public static void copiarArquivo() {
+    }
+
+    public static void main(String[] args) {
+        copiarArquivo();
+    }
+}
