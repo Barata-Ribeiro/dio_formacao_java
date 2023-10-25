c42068f517bc7dae69bf5cad102fbf97c812024d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-25 06:46:15 -0300

    Na prática: I/O Bytes

--- trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/ExemploIOBytes.java
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/ExemploIOBytes.java
@@ -1,13 +1,45 @@
 package br.com.dio.java.io.IOBytes;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 //Faça uma cópia do arquivo “recomendacoes-copy.txt”.
 public class ExemploIOBytes {
-    public static void copiarArquivo() {
+    public static void copiarArquivo() throws IOException {
+        File f = new File("recomendacoes-copy.txt");
+        String fileName = f.getName();
+
+        // InputStream is = new FileInputStream(f.getName());
+        // BufferedInputStream bis = new BufferedInputStream(is);
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f.getName()));
+
+        String fileNameCopy = fileName.substring(0, fileName.indexOf("-")).concat("-copy2.txt");
+        File fCopy = new File(fileNameCopy);
+
+        // OutputStream os = new FileOutputStream(fileNameCopy);
+        // BufferedOutputStream bos = new BufferedOutputStream(os);
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fCopy.getName()));
+
+        int newLine = 0;
+        while (((newLine = bis.read()) != -1)) {
+            bos.write((char) newLine);
+            bos.flush();
+        }
+
+        bis.close();
+        bos.close();
+
+        PrintStream ps = new PrintStream(System.out);
+        ps.printf("Arquivo %s copiado com sucesso e está no diretório '%s' com %d bytes.",
+                fCopy.getName(), fCopy.getAbsolutePath(), fCopy.length());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         copiarArquivo();
     }
 }
