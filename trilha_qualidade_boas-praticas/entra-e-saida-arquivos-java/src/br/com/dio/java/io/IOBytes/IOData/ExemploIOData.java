c865dc9cd13c95c9c6af567634329bf001479dae
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-27 06:52:29 -0300

    Na prática: I/O Data - Tratamentos

--- trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOData/ExemploIOData.java
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOData/ExemploIOData.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 /*
@@ -18,34 +20,52 @@ public class ExemploIOData {
 
         File f = new File("D:\\Armazenamento\\Documentos\\peca-de-roupa.bin");
 
-        PrintStream ps = new PrintStream(System.out);
-        ps.flush();
+        try (PrintStream ps = new PrintStream(System.out);
+                DataOutputStream dos = new DataOutputStream(new FileOutputStream(f.getPath()));
+                Scanner scan = new Scanner(System.in)) {
+            // OutputStream os = new FileOutputStream(f.getPath());
+            // DataOutputStream dos = new DataOutputStream(os);
 
-        // OutputStream os = new FileOutputStream(f.getPath());
-        // DataOutputStream dos = new DataOutputStream(os);
+            scan.useLocale(Locale.US);
 
-        DataOutputStream dos = new DataOutputStream(new FileOutputStream(f.getPath()));
+            ps.print("Nome da peça: ");
+            String nome = scan.nextLine();
+            dos.writeUTF(nome);
 
-        Scanner scan = new Scanner(System.in);
+            ps.print("Tamanho da peça (P/M/G/U): ");
+            char tamanho = scan.next().charAt(0);
+            dos.writeChar(tamanho);
 
-        ps.print("Nome da peça: ");
-        String nome = scan.nextLine();
-        dos.writeUTF(nome);
+            boolean validInput;
 
-        ps.print("Tamanho da peça (P/M/G/U): ");
-        char tamanho = (char) System.in.read();
-        dos.writeChar(tamanho);
+            do {
+                ps.print("Quantidade: ");
+                try {
+                    int quantidade = scan.nextInt();
+                    dos.writeInt(quantidade);
+                    validInput = true;
+                } catch (InputMismatchException e) {
+                    ps.println("Por favor, insira um número inteiro para a quantidade.");
+                    scan.next();
+                    validInput = false;
+                }
+            } while (!validInput);
 
-        ps.print("Quantidade: ");
-        int quantidade = scan.nextInt();
-        dos.writeInt(quantidade);
+            do {
+                ps.print("Preço unitário: ");
+                try {
+                    double preco = scan.nextDouble();
+                    dos.writeDouble(preco);
+                    validInput = true;
+                } catch (InputMismatchException e) {
+                    ps.println("Por favor, insira um número correto para o preço. Exemplo: '49.99'");
+                    scan.next();
+                    validInput = false;
+                }
+            } while (!validInput);
 
-        ps.print("Preço unitário: ");
-        double preco = scan.nextDouble();
-        dos.writeDouble(preco);
-
-        dos.close();
-        scan.close();
+            ps.println("Produto incluído com sucesso!");
+        }
     }
 
     public static void lerProduto(String arquivo) {
