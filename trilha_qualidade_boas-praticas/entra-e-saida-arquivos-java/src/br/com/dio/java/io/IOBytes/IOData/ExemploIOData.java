bc56761a9660ccb3553494a11996431e75a8c2af
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-27 06:40:59 -0300

    Na prática: I/O Data - Parte 1

--- trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOData/ExemploIOData.java
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOData/ExemploIOData.java
@@ -1,5 +1,12 @@
 package br.com.dio.java.io.IOBytes.IOData;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 /*
 Crie um arquivo “peca-de-roupa.bin” e armazene:
 1. Nome do produto, tamanho (P/M/G/U),quantidade e preço.
@@ -7,7 +14,44 @@ Crie um arquivo “peca-de-roupa.bin” e armazene:
 */
 public class ExemploIOData {
 
-    public static void main(String[] args) {
+    public static void incluirProduto() throws IOException {
 
+        File f = new File("D:\\Armazenamento\\Documentos\\peca-de-roupa.bin");
+
+        PrintStream ps = new PrintStream(System.out);
+        ps.flush();
+
+        // OutputStream os = new FileOutputStream(f.getPath());
+        // DataOutputStream dos = new DataOutputStream(os);
+
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(f.getPath()));
+
+        Scanner scan = new Scanner(System.in);
+
+        ps.print("Nome da peça: ");
+        String nome = scan.nextLine();
+        dos.writeUTF(nome);
+
+        ps.print("Tamanho da peça (P/M/G/U): ");
+        char tamanho = (char) System.in.read();
+        dos.writeChar(tamanho);
+
+        ps.print("Quantidade: ");
+        int quantidade = scan.nextInt();
+        dos.writeInt(quantidade);
+
+        ps.print("Preço unitário: ");
+        double preco = scan.nextDouble();
+        dos.writeDouble(preco);
+
+        dos.close();
+        scan.close();
+    }
+
+    public static void lerProduto(String arquivo) {
+    }
+
+    public static void main(String[] args) throws IOException {
+        incluirProduto();
     }
 }
