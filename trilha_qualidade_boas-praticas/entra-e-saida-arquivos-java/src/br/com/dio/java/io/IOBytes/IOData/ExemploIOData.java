d15c5836a29345b61666ef2af4c1bed336e00e89
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-27 06:18:48 -0300

    Na prática: I/O Data - Inicio

---
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOData/ExemploIOData.java
@@ -0,0 +1,13 @@
+package br.com.dio.java.io.IOBytes.IOData;
+
+/*
+Crie um arquivo “peca-de-roupa.bin” e armazene:
+1. Nome do produto, tamanho (P/M/G/U),quantidade e preço.
+2. Leia este arquivo e imprima no console.
+*/
+public class ExemploIOData {
+
+    public static void main(String[] args) {
+
+    }
+}
