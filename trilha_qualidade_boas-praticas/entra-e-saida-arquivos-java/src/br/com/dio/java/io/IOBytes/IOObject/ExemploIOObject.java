240d64620f4ef8f547c0a3a00beeb02a923680c7
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-30 06:56:23 -0300

    Na prática: I/O Objetos - Parte 1

--- trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOObject/ExemploIOObject.java
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOObject/ExemploIOObject.java
@@ -1,12 +1,39 @@
 package br.com.dio.java.io.IOBytes.IOObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 
 //Crie a classe Gato com os atributos nome, idade e cor:
 //Instancie um objeto e serialize
 //Desserialize este objeto e mostre no console
 public class ExemploIOObject {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
+    public static void serealizacao() throws IOException {
+        Gato gato = new Gato("Charlie", 5, "Preto");
+
+        File f = new File("gato.txt");
+
+        // OutputStream os = new FileOutputStream(f.getName());
+        // ObjectOutputStream oos = new ObjectOutputStream(os);
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f.getName()));
+
+        oos.writeObject(gato);
+
+        PrintStream ps = new PrintStream(System.out);
+        ps.printf("Arquivo \"%s\" criado com sucesso!\n Tamanho '%d' bytes.\n", f.getName(), f.length());
+
+        oos.close();
+        ps.close();
+    }
+
+    public static void desserializacao(String arquivo) {
+    }
+
+    public static void main(String[] args) throws IOException {
+        serealizacao();
     }
 }
--- trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOObject/Gato.java
+++ trilha_qualidade_boas-praticas/entra-e-saida-arquivos-java/src/br/com/dio/java/io/IOBytes/IOObject/Gato.java
@@ -4,4 +4,69 @@ import java.io.Serializable;
 
 //nome, idade, cor;
 public class Gato implements Serializable {
+    private String nome;
+    private int idade;
+    private String cor;
+
+    /**
+     * @param nome
+     * @param idade
+     * @param cor
+     */
+    public Gato(String nome, int idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public Gato() {
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the cor
+     */
+    public String getCor() {
+        return cor;
+    }
+
+    /**
+     * @param cor the cor to set
+     */
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    @Override
+    public String toString() {
+        return "Gato [nome=" + nome + ", idade=" + idade + ", cor=" + cor + "]";
+    }
+
 }
