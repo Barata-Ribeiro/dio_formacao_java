9733c2fb3739f74d65870d41eb77c436600925f9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-06 06:46:38 -0300

    Teste JUnit no Maven

---
+++ trilha_qualidade_boas-praticas/testes-unitarios-JUnit/TestesJUnit-maven/src/test/java/com/barataribeiro/PessoaTest.java
@@ -0,0 +1,29 @@
+package com.barataribeiro;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PessoaTest {
+  @Test
+  void deveCalcularIdadeCorretamente() {
+    Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+    Assertions.assertEquals(23, jessica.getIdade());
+  }
+
+  @Test
+  void deveRetornarSeEhMaiorDeIdade() {
+    Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+    Assertions.assertTrue(jessica.ehMaiorDeIdade());
+
+    Pessoa joao = new Pessoa("João", LocalDateTime.now());
+    Assertions.assertFalse(joao.ehMaiorDeIdade());
+  }
+
+  @Test
+  void validaIgualdade() {
+    Pessoa pessoa = new Pessoa("Pessoa 1", LocalDateTime.now());
+    Assertions.assertSame(pessoa, pessoa);
+  }
+}
