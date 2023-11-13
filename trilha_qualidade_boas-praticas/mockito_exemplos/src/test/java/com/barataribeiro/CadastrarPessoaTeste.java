81536c53015176da4f776f6f29e19baa8fe55539
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-13 06:20:48 -0300

    Mockando Objetos

---
+++ trilha_qualidade_boas-praticas/mockito_exemplos/src/test/java/com/barataribeiro/CadastrarPessoaTeste.java
@@ -0,0 +1,36 @@
+package com.barataribeiro;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CadastrarPessoaTeste {
+
+  @Mock
+  private ApiDosCorreios apiDosCorreios;
+
+  @InjectMocks
+  private CadastrarPessoa cadastrarPessoa;
+
+  @Test
+  void validarDadosDeCadastro() {
+    DadosLocalizacao dadosLocalizacao = new DadosLocalizacao("RO", "Porto Velho", "Rua Pintado", "Num. 171 - Ap. 305",
+        "Lagoa");
+
+    Mockito.when(apiDosCorreios.buscaDadosComBaseNoCep("76812-208")).thenReturn(dadosLocalizacao);
+
+    Pessoa pessoaUm = cadastrarPessoa.cadastrarPessoa("Jason Bourne", "24.260.056-6", LocalDate.now(), "76812-208");
+
+    Assertions.assertEquals("Jason Bourne", pessoaUm.getNome());
+    Assertions.assertEquals("24.260.056-6", pessoaUm.getDocumento());
+    Assertions.assertEquals("RO", pessoaUm.getEndereco().getUf());
+    Assertions.assertEquals("Num. 171 - Ap. 305", pessoaUm.getEndereco().getComplemento());
+  }
+}
