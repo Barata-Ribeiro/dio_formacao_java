52691fea752fdd5435cbb63f1b3ead939c294ba5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-14 06:25:25 -0300

    Manipulando Retornos

--- trilha_qualidade_boas-praticas/mockito_exemplos/src/test/java/com/barataribeiro/CadastrarPessoaTeste.java
+++ trilha_qualidade_boas-praticas/mockito_exemplos/src/test/java/com/barataribeiro/CadastrarPessoaTeste.java
@@ -1,5 +1,7 @@
 package com.barataribeiro;
 
+import static org.mockito.ArgumentMatchers.anyString;
+
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Assertions;
@@ -28,9 +30,23 @@ public class CadastrarPessoaTeste {
 
     Pessoa pessoaUm = cadastrarPessoa.cadastrarPessoa("Jason Bourne", "24.260.056-6", LocalDate.now(), "76812-208");
 
+    Assertions.assertNull(pessoaUm.getEndereco());
     Assertions.assertEquals("Jason Bourne", pessoaUm.getNome());
     Assertions.assertEquals("24.260.056-6", pessoaUm.getDocumento());
     Assertions.assertEquals("RO", pessoaUm.getEndereco().getUf());
     Assertions.assertEquals("Num. 171 - Ap. 305", pessoaUm.getEndereco().getComplemento());
+  }
+
+  @Test
+  void lancarExceptionQuandoChamarApiDosCorreios() {
+    Mockito.when(apiDosCorreios.buscaDadosComBaseNoCep(anyString()))
+        .thenThrow(IllegalArgumentException.class);
+
+    // Outro método de chamar o mockito
+    // Mockito.doThrow(IllegalArgumentException.class)
+    // .when(apiDosCorreios.buscaDadosComBaseNoCep(anyString()));
+
+    Assertions.assertThrows(IllegalArgumentException.class,
+        () -> cadastrarPessoa.cadastrarPessoa("Jason Bourne", "24.260.056-6", LocalDate.now(), "76812-208"));
   }
 }
