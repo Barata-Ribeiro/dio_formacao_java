42d779fa3ceeee104ae98dd9c4f9301899427182
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-22 06:59:52 -0300

    Beans versus Components - Começando

---
+++ trilha_springframework/spring-ioc-di-beans-autowired/src/main/java/com/barataribeiro/springiocdibeansautowired/SpringIocDiBeansAutowiredApplication.java
@@ -0,0 +1,27 @@
+package com.barataribeiro.springiocdibeansautowired;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.barataribeiro.springiocdibeansautowired.app.ConversorJson;
+import com.barataribeiro.springiocdibeansautowired.app.ViaCepResponse;
+
+@SpringBootApplication
+public class SpringIocDiBeansAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocDiBeansAutowiredApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
+	}
+
+}
