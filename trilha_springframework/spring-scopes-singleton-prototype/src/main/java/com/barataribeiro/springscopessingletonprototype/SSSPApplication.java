d16457521e248822a710d89a55cfbeed429c909f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-23 06:10:00 -0300

    Scopes - Singleton ou Prototype

---
+++ trilha_springframework/spring-scopes-singleton-prototype/src/main/java/com/barataribeiro/springscopessingletonprototype/SSSPApplication.java
@@ -0,0 +1,13 @@
+package com.barataribeiro.springscopessingletonprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSSPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSSPApplication.class, args);
+	}
+
+}
---
+++ trilha_springframework/spring-scopes-singleton-prototype/src/main/java/com/barataribeiro/springscopessingletonprototype/SpringPrimeirosPassosApplication.java
@@ -0,0 +1,26 @@
+package com.barataribeiro.springscopessingletonprototype;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.barataribeiro.springscopessingletonprototype.app.SistemaMensagem;
+
+@SpringBootApplication
+public class SpringPrimeirosPassosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner run(SistemaMensagem sistema) throws Exception {
+    return args -> {
+      sistema.enviarConfirmacaoCadastro();
+      sistema.enviarMensagemBoasVindas();
+      sistema.enviarConfirmacaoCadastro();
+    };
+  }
+
+}
