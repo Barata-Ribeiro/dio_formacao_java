682d88d061d4a9f688c94e5c00eb1eaf7e2e647c
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-29 09:17:39 -0300

    JWT - JSON Web Token - Parte 4

---
+++ trilha_springframework/spring-security-jwt/src/main/java/com/barataribeiro/springsecurityjwt/dtos/Sessao.java
@@ -0,0 +1,35 @@
+package com.barataribeiro.springsecurityjwt.dtos;
+
+public class Sessao {
+  private String login;
+  private String token;
+
+  /**
+   * @return the login
+   */
+  public String getLogin() {
+    return login;
+  }
+
+  /**
+   * @param login the login to set
+   */
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  /**
+   * @return the token
+   */
+  public String getToken() {
+    return token;
+  }
+
+  /**
+   * @param token the token to set
+   */
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+}
