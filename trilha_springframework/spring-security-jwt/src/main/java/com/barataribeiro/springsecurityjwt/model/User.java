817bfb0fdba114ea11e5499d264bdbd41f92713d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-29 08:57:24 -0300

    JWT - JSON Web Token - Parte 3

--- trilha_springframework/spring-security-jwt/src/main/java/com/barataribeiro/springsecurityjwt/model/User.java
+++ trilha_springframework/spring-security-jwt/src/main/java/com/barataribeiro/springsecurityjwt/model/User.java
@@ -3,16 +3,16 @@ package com.barataribeiro.springsecurityjwt.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tab_user")
