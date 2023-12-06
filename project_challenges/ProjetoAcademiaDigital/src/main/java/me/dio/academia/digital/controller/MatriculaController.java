e70bc74d89f9e0d8933f796aecc6a91dda803f8e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-06 07:04:13 -0300

    Implementado rota matrícula

--- project_challenges/ProjetoAcademiaDigital/src/main/java/me/dio/academia/digital/controller/MatriculaController.java
+++ project_challenges/ProjetoAcademiaDigital/src/main/java/me/dio/academia/digital/controller/MatriculaController.java
@@ -1,9 +1,43 @@
 package me.dio.academia.digital.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import me.dio.academia.digital.entity.Matricula;
+import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.service.impl.MatriculaServiceImpl;
 
 @RestController
 @RequestMapping("/matriculas")
 public class MatriculaController {
+  @Autowired
+  private MatriculaServiceImpl service;
+
+  @PostMapping
+  public Matricula createMatricula(@RequestBody MatriculaForm form) {
+    return service.create(form);
+  }
+
+  @GetMapping("/{id}")
+  public Matricula getMatriculaById(@PathVariable Long id) {
+    return service.get(id);
+  }
+
+  @GetMapping
+  public List<Matricula> getAllMatriculas() {
+    return service.getAll();
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteMatricula(@PathVariable Long id) {
+    service.delete(id);
+  }
 }
---
+++ project_challenges/ProjetoAcademiaDigital/src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -0,0 +1,46 @@
+package me.dio.academia.digital.service.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import me.dio.academia.digital.entity.Matricula;
+import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.MatriculaRepository;
+import me.dio.academia.digital.service.IMatriculaService;
+
+public class MatriculaServiceImpl implements IMatriculaService {
+
+  @Autowired
+  private MatriculaRepository matriculaRepository;
+
+  @Autowired
+  private AlunoRepository alunoRepository;
+
+  @Override
+  public Matricula create(MatriculaForm form) {
+    Matricula matricula = new Matricula();
+    matricula.setAluno(alunoRepository.findById(form.getAlunoId())
+        .orElseThrow(() -> new EntityNotFoundException("Aluno não encontrado")));
+    return matriculaRepository.save(matricula);
+  }
+
+  @Override
+  public Matricula get(Long id) {
+    return matriculaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Matrícula não encontrada"));
+  }
+
+  @Override
+  public List<Matricula> getAll() {
+    return matriculaRepository.findAll();
+  }
+
+  @Override
+  public void delete(Long id) {
+    matriculaRepository.deleteById(id);
+  }
+
+}
